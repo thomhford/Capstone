ab765549bfbbaa684b4c6e80153793a4dc8eb580
Thomas Ford <u0449765@utah.edu>
2023-09-11 21:30:53 -0600

    got navBar implemented and working

---
+++ Application/msd_capstone/lib/app/screens/upload_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class UploadPage extends StatelessWidget {
+  const UploadPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Upload Page'),
+      ),
+      body: const Center(
+        child: Text('Upload Page'),
+      ),
+    );
+  }
+}
