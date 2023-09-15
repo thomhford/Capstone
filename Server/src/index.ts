75a3e4d919800acbe37da962df3ee01665cc9caa
thomasford <u0449765@utah.edu>
2023-09-14 19:34:47 -0600

    removed secret key generator

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,14 +1,9 @@
 import express from 'express';
 import sequelize from "../config/db";
 import userRoutes from "./routes/user";
-import 'dotenv/config';
-import {secretKey} from "./utils/secretKey";
 
 const app = express();
 const port = 3000;
-
-// Set JWT secret key for signing and verifying tokens
-process.env.JWT_SECRET = secretKey;
 
 sequelize.sync().then(() => {
     console.log('Database & tables created!');
