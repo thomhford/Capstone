45acca950e72ae1d4b995fc23e57e6a4a0a151c4
Thomas Ford <u0449765@utah.edu>
2023-09-17 21:11:14 -0600

    small updates

--- Server/src/routes/user.ts
+++ Server/src/routes/user.ts
@@ -6,11 +6,17 @@ import admin from "firebase-admin";
 const router = express.Router();
 
 router.post('/login', async (req, res) => {
-    const { idToken } = req.body;
+    const idToken = req.headers.authorization?.split('Bearer ')[1];
+
+    if (!idToken) {
+        return res.status(401).json({ message: 'Unauthorized request' });
+    }
 
     try {
         const decodedToken = await admin.auth().verifyIdToken(idToken);
         const uid = decodedToken.uid;
+
+        let user = await User.findOne({ where: { uid } });
 
         res.status(200).json({ message: 'Logged in successfully', uid });
     } catch (error) {
@@ -19,7 +25,6 @@ router.post('/login', async (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-    // Get the ID token from the Authorization header
     const idToken = req.headers.authorization?.split('Bearer ')[1];
 
     if (!idToken) {
@@ -27,24 +32,18 @@ router.post('/register', async (req, res) => {
     }
 
     try {
-        // Verify the ID token and get the uid of the user
         const decodedToken = await admin.auth().verifyIdToken(idToken);
         const uid = decodedToken.uid;
 
-        // Validate the user in Firebase Authentication
-        const userRecord = await admin.auth().getUser(uid);
+        let user = await User.findOne({ where: { uid } });
 
-        // Check if the user already exists in your database
-        let user = await User.findOne({ where: { uid: userRecord.uid } });
-
-        // If the user does not exist in your database, create a new user
         if (!user) {
             const { firstName, lastName, email } = req.body;
-            user = await User.create({
+            await User.create({
                 firstName,
                 lastName,
                 email,
-                uid: userRecord.uid, // Store the Firebase UID
+                uid
             });
         }
 
