489b98d075751a38d7b8f3a4f5e2a77d04bf8b2c
thomasford <u0449765@utah.edu>
2023-09-19 18:39:27 -0600

    added logging statments to server

--- Server/src/routes/user.ts
+++ Server/src/routes/user.ts
@@ -25,34 +25,49 @@ router.post('/login', async (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-    console.log("received request")
+    console.log("req.headers:", req.headers)
     const idToken = req.headers.authorization?.split('Bearer ')[1];
+    console.log("idToken:", idToken)
+    console.log("req.headers:", req.headers)
+    console.log("req.body:", req.body)
 
     if (!idToken) {
+        console.log("Unauthorized request")
         return res.status(401).json({ message: 'Unauthorized request' });
     }
 
     try {
-        const decodedToken = await admin.auth().verifyIdToken(idToken);
+        let decodedToken;
+        try {
+            decodedToken = await admin.auth().verifyIdToken(idToken);
+        } catch (error) {
+            console.log("Cannot verify token:", error)
+            return res.status(400).json({ message: error });
+        }
         const uid = decodedToken.uid;
+        console.log("uid:", uid)
 
         let user = await User.findOne({ where: { uid } });
 
         if (user) {
+            console.log("user already exists")
             return res.status(400).json({ message: 'User already exists' });
         } else {
             const { firstName, lastName, email } = req.body;
+            console.log("creating user", firstName, lastName, email, uid)
             await User.create({
                 firstName,
                 lastName,
                 email,
                 uid
             });
+            console.log("user registered successfully")
             return res.status(200).json({ message: 'User registered successfully' });
         }
 
     } catch (error: any) {
-        res.status(400).json({ message: error.message });
+        console.log(error)
+        return res.status(400).json({ message: error.message });
     }
 });
 
