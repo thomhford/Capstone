4f7f6fda51dc2c42254e06a849832f7fe7fc3969
thomasford <u0449765@utah.edu>
2023-09-21 00:44:16 -0600

    fixed obscureText toggle

--- Application/msd_capstone/lib/app/components/main_textfield.dart
+++ Application/msd_capstone/lib/app/components/main_textfield.dart
@@ -1,24 +1,47 @@
 import 'package:flutter/material.dart';
 
-class MainTextField extends StatelessWidget {
-  final controller;
+class MainTextField extends StatefulWidget {
+  final TextEditingController controller;
   final String hintText;
   final bool obscureText;
+  final FormFieldValidator<String>? validator;
+  final Widget? suffixIcon;
 
   const MainTextField({
-    super.key,
+    Key? key,
     required this.controller,
     required this.hintText,
     required this.obscureText,
-  });
+    this.validator,
+    this.suffixIcon,
+  }) : super(key: key);
+
+  @override
+  MainTextFieldState createState() => MainTextFieldState();
+}
+
+class MainTextFieldState extends State<MainTextField> {
+  bool _obscureText = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscureText = widget.obscureText;
+  }
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 25.0),
-      child: TextField(
-        controller: controller,
-        obscureText: obscureText,
+      child: TextFormField(
+        controller: widget.controller,
+        obscureText: _obscureText,
         decoration: InputDecoration(
           enabledBorder: const OutlineInputBorder(
             borderSide: BorderSide(color: Colors.white),
@@ -28,9 +51,19 @@ class MainTextField extends StatelessWidget {
           ),
           fillColor: Colors.grey.shade200,
           filled: true,
-          hintText: hintText,
+          hintText: widget.hintText,
           hintStyle: TextStyle(color: Colors.grey[500]),
+          suffixIcon: widget.suffixIcon ??
+              (widget.obscureText
+                  ? IconButton(
+                      icon: Icon(
+                        _obscureText ? Icons.visibility : Icons.visibility_off,
+                      ),
+                      onPressed: _togglePasswordVisibility,
+                    )
+                  : null),
         ),
+        validator: widget.validator,
       ),
     );
   }
