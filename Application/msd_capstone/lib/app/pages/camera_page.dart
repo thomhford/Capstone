9747947e135ac513d13c4b8dc1e5d6d123d7081c
Thomas Ford <u0449765@utah.edu>
2023-10-11 23:02:36 -0600

    updates for camera usage

--- Application/msd_capstone/lib/app/pages/camera_page.dart
+++ Application/msd_capstone/lib/app/pages/camera_page.dart
@@ -1,259 +1,58 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+
+final logger = Logger();
 
 class CameraPage extends StatefulWidget {
-  const CameraPage({super.key});
+  const CameraPage({Key? key}) : super(key: key);
 
   @override
   _CameraPageState createState() => _CameraPageState();
 }
 
 class _CameraPageState extends State<CameraPage> {
-  late List<CameraDescription> _cameras;
-  late CameraController _controller;
-  bool _isReady = false;
+  CameraController? _controller;
+  List<CameraDescription>? cameras;
+  bool isCameraReady = false;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    _setUpCamera();
+    initializeCamera();
   }
 
-  void _setUpCamera() async {
+  Future<void> initializeCamera() async {
+    cameras = await availableCameras();
+    _controller = CameraController(cameras![0], ResolutionPreset.veryHigh);
     try {
-      // initialize cameras.
-      _cameras = await availableCameras();
-      // initialize camera controllers.
-      // Current bug for high / medium with samsung devices.
-      _controller = CameraController(
-        _cameras[0],
-        ResolutionPreset.medium,
-      );
-
-      await _controller.initialize();
-    } on CameraException catch (_) {
-      // do something on error.
+      await _controller!.initialize();
+      if (mounted) {
+        setState(() {
+          isCameraReady = true;
+        });
+      }
+    } catch (e) {
+      logger.e("Failed to initialize camera: $e");
     }
-    if (!mounted) return;
-    setState(() {
-      _isReady = true;
-    });
+  }
+
+  @override
+  void dispose() {
+    _controller!.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      floatingActionButton: getFooter(),
-      body: getBody(),
-    );
-  }
-
-  Widget cameraPreview() {
-    return AspectRatio(
-        aspectRatio: _controller.value.aspectRatio,
-        child: CameraPreview(_controller));
-  }
-
-  Widget getBody() {
-    var size = MediaQuery.of(context).size;
-    if (_isReady == false ||
-        _controller == null ||
-        !_controller.value.isInitialized) {
-      return Container(
-        decoration: const BoxDecoration(color: Colors.white),
-        width: size.width,
-        height: size.height,
-        child: const Center(
-            child: SizedBox(
-                width: 25,
-                height: 25,
-                child: CircularProgressIndicator(
-                  strokeWidth: 3,
-                ))),
+    if (!isCameraReady) {
+      return const Center(child: CircularProgressIndicator());
+    } else {
+      return AspectRatio(
+        aspectRatio: _controller!.value.aspectRatio,
+        child: CameraPreview(_controller!),
       );
     }
-
-    return Container(
-      width: size.width,
-      height: size.height,
-      child: ClipRRect(
-          borderRadius: const BorderRadius.only(
-              bottomLeft: Radius.circular(10),
-              bottomRight: Radius.circular(10)),
-          child: cameraPreview()),
-    );
-  }
-
-  Widget getBodyBK() {
-    var size = MediaQuery.of(context).size;
-    return Container(
-      width: size.width,
-      height: size.height,
-      decoration: const BoxDecoration(
-          borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(20),
-              bottomRight: Radius.circular(20)),
-          color: Colors.white),
-      child: const Image(
-        image: NetworkImage(
-          "https://images.unsplash.com/photo-1582152629442-4a864303fb96?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c2VsZmllfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
-        ),
-        fit: BoxFit.cover,
-      ),
-    );
-  }
-
-  Widget getFooter() {
-    return Padding(
-      padding: const EdgeInsets.only(left: 30, top: 60),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.black.withOpacity(0.3)),
-                    child: const Icon(
-                      Icons.account_circle,
-                      color: Colors.yellow,
-                      size: 23,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.black.withOpacity(0.3)),
-                    child: const Icon(
-                      Icons.search,
-                      color: Colors.white,
-                      size: 23,
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.black.withOpacity(0.3)),
-                    child: const Icon(
-                      Icons.account_box_outlined,
-                      color: Colors.white,
-                      size: 20,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        color: Colors.black.withOpacity(0.3)),
-                    child: const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          Icon(
-                            Icons.refresh,
-                            color: Colors.white,
-                            size: 25,
-                          ),
-                          SizedBox(
-                            height: 18,
-                          ),
-                          Icon(
-                            Icons.flash_on,
-                            color: Colors.white,
-                            size: 25,
-                          ),
-                          SizedBox(
-                            height: 18,
-                          ),
-                          Icon(
-                            Icons.play_circle_outlined,
-                            color: Colors.white,
-                            size: 25,
-                          ),
-                          SizedBox(
-                            height: 18,
-                          ),
-                          Icon(
-                            Icons.music_note_outlined,
-                            color: Colors.white,
-                            size: 25,
-                          ),
-                          SizedBox(
-                            height: 18,
-                          ),
-                          Icon(
-                            CupertinoIcons.arrow_down,
-                            color: Colors.white,
-                            size: 25,
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const IconButton(
-                onPressed: null,
-                icon: Icon(
-                  Icons.games_sharp,
-                  color: Colors.white,
-                  size: 28,
-                ),
-              ),
-              const SizedBox(
-                width: 15,
-              ),
-              Container(
-                width: 80,
-                height: 80,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(width: 7, color: Colors.white)),
-              ),
-              const SizedBox(
-                width: 15,
-              ),
-              const IconButton(
-                onPressed: null,
-                icon: Icon(
-                  Icons.emoji_emotions_outlined,
-                  color: Colors.white,
-                  size: 28,
-                ),
-              )
-            ],
-          )
-        ],
-      ),
-    );
   }
 }
