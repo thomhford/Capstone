f2f8421a49f07494b42cce941b930ee5101e613a
thomasford <u0449765@utah.edu>
2023-10-30 13:39:35 -0600

    updates I dont want to lose

--- Application/msd_capstone/lib/chat/chats_page.dart
+++ Application/msd_capstone/lib/chat/chats_page.dart
@@ -8,14 +8,17 @@ class ChatsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF1B202D),
+      backgroundColor: const Color(0xFF1B202D),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 15.0, right: 15.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 10,
+                right: 10,
+              ),
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   const Text(
@@ -30,68 +33,281 @@ class ChatsPage extends StatelessWidget {
                   IconButton(
                     onPressed: () {},
                     icon: const Icon(
-                      Icons.search,
+                      Icons.create,
                       color: Colors.white,
                     ),
                   ),
                 ],
               ),
-              const SizedBox(
-                height: 5,
-              ),
-              const Text(
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text(
                 'R E C E N T',
                 style: TextStyle(
                   color: Colors.white,
                 ),
               ),
-              const SizedBox(
-                height: 15,
-              ),
-              const Row(
-                children: [
-                  RecentUsers(),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 100,
+              child: RecentUsers(
+                users: [
+                  User(
+                      name: 'John Smith',
+                      imageUrl: 'https://i.imgur.com/TzEia5G.jpeg'),
+                  User(
+                      name: 'Steve Johnson',
+                      imageUrl: 'https://i.imgur.com/TzEia5G.jpeg'),
+                  User(
+                      name: 'John',
+                      imageUrl: 'https://i.imgur.com/TzEia5G.jpeg'),
+                  User(
+                      name: 'John',
+                      imageUrl: 'https://i.imgur.com/TzEia5G.jpeg'),
+                  User(
+                      name: 'John',
+                      imageUrl: 'https://i.imgur.com/TzEia5G.jpeg'),
+                  User(
+                      name: 'John',
+                      imageUrl: 'https://i.imgur.com/TzEia5G.jpeg'),
                 ],
               ),
-            ],
-          ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 600,
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                ),
+                color: Color(0xFF292F3F),
+              ),
+              child: RecentMessages(
+                messages: [
+                  Message(
+                    user: User(
+                      name: 'John',
+                      imageUrl: 'https://i.imgur.com/TzEia5G.jpeg',
+                    ),
+                    text: 'Hey, how\'s it going?',
+                    timestamp: '2:30 PM',
+                  ),
+                  Message(
+                    user: User(
+                      name: 'John',
+                      imageUrl: 'https://i.imgur.com/TzEia5G.jpeg',
+                    ),
+                    text: 'Hey, how\'s it going?',
+                    timestamp: '2:30 PM',
+                  ),
+                  Message(
+                    user: User(
+                      name: 'John',
+                      imageUrl: 'https://i.imgur.com/TzEia5G.jpeg',
+                    ),
+                    text: 'Hey, how\'s it going?',
+                    timestamp: '2:30 PM',
+                  ),
+                  Message(
+                    user: User(
+                      name: 'John',
+                      imageUrl: 'https://i.imgur.com/TzEia5G.jpeg',
+                    ),
+                    text: 'Hey, how\'s it going?',
+                    timestamp: '2:30 PM',
+                  ),
+                  Message(
+                    user: User(
+                      name: 'John Smith',
+                      imageUrl: 'https://i.imgur.com/TzEia5G.jpeg',
+                    ),
+                    text: 'Hey, how\'s it going?',
+                    timestamp: '2:30 PM',
+                  ),
+                  Message(
+                    user: User(
+                      name: 'John',
+                      imageUrl: 'https://i.imgur.com/TzEia5G.jpeg',
+                    ),
+                    text: 'Hey, how\'s it going hhhhhhhhhhhhhh?',
+                    timestamp: '2:30 PM',
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
-class RecentUsers extends StatelessWidget {
-  const RecentUsers({
-    super.key,
-  });
+class RecentMessages extends StatelessWidget {
+  final List<Message> messages;
+
+  const RecentMessages({
+    Key? key,
+    required this.messages,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+    return ListView.builder(
+      itemCount: messages.length,
+      itemBuilder: (context, index) {
+        final message = messages[index];
+        return Padding(
+          padding: const EdgeInsets.only(left: 20, right: 10, top: 25),
+          child: GestureDetector(
+            onTap: () {},
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: NetworkImage(message.user.imageUrl),
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.5,
+                          child: Text(
+                            message.user.name,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Quicksand',
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.2,
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Text(
+                              message.timestamp,
+                              style: const TextStyle(
+                                color: Colors.white70,
+                                fontFamily: 'Quicksand',
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 2,
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      child: Text(
+                        message.text,
+                        style: const TextStyle(
+                          color: Colors.white70,
+                          fontFamily: 'Quicksand',
+                        ),
+                        overflow: TextOverflow.fade,
+                        maxLines: 1,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class RecentUsers extends StatelessWidget {
+  final List<User> users;
+
+  const RecentUsers({
+    Key? key,
+    required this.users,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: users.length,
+      itemBuilder: (context, index) {
+        return Row(
           children: [
-            CircleAvatar(
-              radius: 30,
+            const SizedBox(
+              width: 25,
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Name',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Quicksand',
+            GestureDetector(
+              onTap: () {},
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundImage: NetworkImage(users[index].imageUrl),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    users[index].name.split(' ')[0],
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Quicksand',
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
-        ),
-      ],
+        );
+      },
     );
   }
+}
+
+class User {
+  final String name;
+  final String imageUrl;
+
+  User({
+    required this.name,
+    required this.imageUrl,
+  });
+}
+
+class Message {
+  final User user;
+  final String text;
+  final String timestamp;
+
+  Message({
+    required this.user,
+    required this.text,
+    required this.timestamp,
+  });
 }
