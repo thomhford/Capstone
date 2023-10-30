60880d66e6325b9d07ef4f37beb7362f60ff1d5e
thomasford <u0449765@utah.edu>
2023-10-30 11:00:38 -0600

    added dependencies and fonts

--- Application/msd_capstone/lib/chat/chats_page.dart
+++ Application/msd_capstone/lib/chat/chats_page.dart
@@ -8,12 +8,90 @@ class ChatsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Chats Page'),
+      backgroundColor: Color(0xFF1B202D),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Messages',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Quicksand',
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              const Text(
+                'R E C E N T',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Row(
+                children: [
+                  RecentUsers(),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-      body: const Center(
-        child: Text('Chats Page'),
-      ),
+    );
+  }
+}
+
+class RecentUsers extends StatelessWidget {
+  const RecentUsers({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 30,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Name',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Quicksand',
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
--- Application/msd_capstone/lib/main.dart
+++ Application/msd_capstone/lib/main.dart
@@ -3,24 +3,12 @@
 // import 'package:flutter/material.dart';
 // import 'package:firebase_core/firebase_core.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:msd_capstone/sign_up/register_page.dart';
 // import 'firebase_options.dart';
-// import 'components/navbar.dart';
-// import 'login/login_page.dart';
-// import 'theme.dart';
 
 // import 'camera/camera_page.dart';
 // import 'post/upload_page.dart';
 // import 'profile/profile_page.dart';
 // import 'search/search_page.dart';
-
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
-//   runApp(const MyAppTest());
-// }
 
 // class MyApp extends StatelessWidget {
 //   const MyApp({Key? key}) : super(key: key);
@@ -48,37 +36,51 @@
 //   }
 // }
 
-// // For Testing Widgets
-// class MyAppTest extends StatelessWidget {
-//   const MyAppTest({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       theme: lightMode,
-//       darkTheme: darkMode,
-//       home: const RegisterPage(),
-//     );
-//   }
-// }
-
-import 'package:authentication_repository/authentication_repository.dart';
-import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:msd_capstone/chat/chats_page.dart';
+import 'theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:flutter/widgets.dart';
-import 'app/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Bloc.observer = const AppBlocObserver();
-
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
-  final authenticationRepository = AuthenticationRepository();
-  await authenticationRepository.user.first;
-
-  runApp(App(authenticationRepository: authenticationRepository));
+  runApp(const MyAppTest());
 }
+
+// For Testing Widgets
+class MyAppTest extends StatelessWidget {
+  const MyAppTest({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: lightMode,
+      darkTheme: darkMode,
+      home: const ChatsPage(),
+    );
+  }
+}
+
+// import 'package:authentication_repository/authentication_repository.dart';
+// import 'package:bloc/bloc.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
+// import 'package:flutter/widgets.dart';
+// import 'app/app.dart';
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   Bloc.observer = const AppBlocObserver();
+
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+
+//   final authenticationRepository = AuthenticationRepository();
+//   await authenticationRepository.user.first;
+
+//   runApp(App(authenticationRepository: authenticationRepository));
+// }
