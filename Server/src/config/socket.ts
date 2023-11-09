1d784dbfdbd09fa6552e2b3c73113b212ad91c72
Thomas Ford <u0449765@utah.edu>
2023-11-09 14:44:02 -0700

    added events to socket config

--- Server/src/config/socket.ts
+++ Server/src/config/socket.ts
@@ -7,6 +7,32 @@ const io = new Server(server);
 
 io.on('connection', (socket) => {
     console.log('a user connected', socket.id);
+
+    // Handle 'chat message' event
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        // Broadcast the message to all other clients
+        socket.broadcast.emit('chat message', msg);
+    });
+
+    // Handle 'join room' event
+    socket.on('join room', (room) => {
+        console.log('user joined room: ' + room);
+        // Add the client to the room
+        socket.join(room);
+        // Broadcast a message to all clients in the room
+        io.to(room).emit('chat message', 'a user has joined the room');
+    });
+
+    // Handle 'leave room' event
+    socket.on('leave room', (room) => {
+        console.log('user left room: ' + room);
+        // Remove the client from the room
+        socket.leave(room);
+        // Broadcast a message to all clients in the room
+        io.to(room).emit('chat message', 'a user has left the room');
+    });
+
     socket.on('disconnect', () => {
         console.log('user disconnected', socket.id)
     })
