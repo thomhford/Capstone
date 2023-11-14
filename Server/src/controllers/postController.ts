fa2d7e62b5a860479460edb09a2522cb3c4f4abd
thomasford <u0449765@utah.edu>
2023-11-13 19:38:59 -0700

    added File to query of Posts

--- Server/src/controllers/postController.ts
+++ Server/src/controllers/postController.ts
@@ -1,12 +1,17 @@
 // postController.ts
 
 import { Request, Response } from 'express';
-import { Post } from '../models';
+import { Post, File } from '../models';
 import {getUserId} from "../utils/authUtils";
 
 export const handlePostRequest = async (_req: Request, res: Response) => {
     try {
         const posts = await Post.findAll({
+            include: [{
+                model: File,
+                as: 'Files'
+
+            }],
             order : [['upload_date', 'DESC']]
         });
         return res.status(200).json(posts);
@@ -23,6 +28,10 @@ export const handleUserPostRequest = async (req: Request, res: Response) => {
             where: {
                 userId: userId
             },
+            include: [{
+                model: File,
+                as: 'Files'
+            }],
             order: [['upload_date', 'DESC']]
         });
         return res.status(200).json(posts);
