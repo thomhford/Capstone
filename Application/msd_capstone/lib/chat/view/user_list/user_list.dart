4a3768d244c50e460572894ed2d5e3cc964b8f55
thomasford <u0449765@utah.edu>
2023-12-04 15:15:47 -0700

    fixed issue with state not being correctly listened for

--- Application/msd_capstone/lib/chat/view/user_list/user_list.dart
+++ Application/msd_capstone/lib/chat/view/user_list/user_list.dart
@@ -18,21 +18,6 @@ class UserList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Listen for the new conversation
-    BlocListener<ChatBloc, ChatState>(
-      listener: (context, state) {
-        if (state is SocketNewConversationReceived) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => ConversationPage(
-                  conversation: state.conversation),
-            ),
-          );
-        }
-      },
-      child: const Center(child: CircularProgressIndicator()),
-    );
     final List<ChatUser> filteredUserList = users.where((user) {
       final String query = searchQuery.toLowerCase();
       return user.firstName.toLowerCase().contains(query) ||
@@ -58,93 +43,113 @@ class UserList extends StatelessWidget {
         ),
       );
     }
-
-    return ListView.builder(
-      itemCount: filteredUserList.length,
-      itemBuilder: (context, index) {
-        final user = filteredUserList[index];
-        return Container(
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(40),
-              topRight: Radius.circular(40),
-              bottomLeft: Radius.circular(40),
-              bottomRight: Radius.circular(40),
+    // Listen for the new conversation
+    return BlocListener<ChatBloc, ChatState>(
+      listener: (context, state) {
+        if (state is SocketNewConversationReceived) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) =>
+                  ConversationPage(conversation: state.conversation),
             ),
-            color: theme.colorScheme.secondary,
-          ),
-          child: ListTile(
-            title: Text(
-              '${user.firstName} ${user.lastName}',
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Quicksand',
-              ),
+          );
+        } else if (state is SocketError) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(state.error),
+              backgroundColor: Colors.red,
             ),
-            leading: Avatar(
-              photo: user.photoUrl,
-              name: '${user.firstName} ${user.lastName}',
-              avatarSize: 25,
-            ),
-            subtitle: Text(
-              user.email,
-              style: const TextStyle(
-                color: Colors.white70,
-                fontSize: 15,
-                fontFamily: 'Quicksand',
-              ),
-            ),
-            onTap: () {
-              final chatBloc = BlocProvider.of<ChatBloc>(context);
-              final currentUser = chatBloc.currentUserId;
-              final existingConversation =
-                  _chatBloc.chatData.conversations.values.firstWhere(
-                      (conversation) =>
-                          (conversation.user1.id == currentUser &&
-                              conversation.user2.id == user.id) ||
-                          (conversation.user1.id == user.id &&
-                              conversation.user2.id == currentUser),
-                      orElse: () => Conversation(
-                            conversationId: -1,
-                            user1: ChatUser(
-                                id: '',
-                                firstName: '',
-                                lastName: '',
-                                email: '',
-                                updatedAt: DateTime.now(),
-                                photoUrl: ''),
-                            user2: ChatUser(
-                                id: '',
-                                firstName: '',
-                                lastName: '',
-                                email: '',
-                                updatedAt: DateTime.now(),
-                                photoUrl: ''),
-                            messages: {},
-                            createdAt: DateTime.now(),
-                          ));
-              if (existingConversation.conversationId == -1) {
-                // Create new conversation
-                chatBloc.add(
-                  NewConversationEvent(currentUser, user.id),
-                );
-              } else {
-                // Open existing conversation
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        ConversationPage(conversation: existingConversation),
-                  ),
-                );
-              }
-            },
-          ),
-        );
+          );
+        }
       },
+      child: ListView.builder(
+        itemCount: filteredUserList.length,
+        itemBuilder: (context, index) {
+          final user = filteredUserList[index];
+          return Container(
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(40),
+                topRight: Radius.circular(40),
+                bottomLeft: Radius.circular(40),
+                bottomRight: Radius.circular(40),
+              ),
+              color: theme.colorScheme.secondary,
+            ),
+            child: ListTile(
+              title: Text(
+                '${user.firstName} ${user.lastName}',
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Quicksand',
+                ),
+              ),
+              leading: Avatar(
+                photo: user.photoUrl,
+                name: '${user.firstName} ${user.lastName}',
+                avatarSize: 25,
+              ),
+              subtitle: Text(
+                user.email,
+                style: const TextStyle(
+                  color: Colors.white70,
+                  fontSize: 15,
+                  fontFamily: 'Quicksand',
+                ),
+              ),
+              onTap: () {
+                final chatBloc = BlocProvider.of<ChatBloc>(context);
+                final currentUser = chatBloc.currentUserId;
+                final existingConversation =
+                    _chatBloc.chatData.conversations.values.firstWhere(
+                        (conversation) =>
+                            (conversation.user1.id == currentUser &&
+                                conversation.user2.id == user.id) ||
+                            (conversation.user1.id == user.id &&
+                                conversation.user2.id == currentUser),
+                        orElse: () => Conversation(
+                              conversationId: -1,
+                              user1: ChatUser(
+                                  id: '',
+                                  firstName: '',
+                                  lastName: '',
+                                  email: '',
+                                  updatedAt: DateTime.now(),
+                                  photoUrl: ''),
+                              user2: ChatUser(
+                                  id: '',
+                                  firstName: '',
+                                  lastName: '',
+                                  email: '',
+                                  updatedAt: DateTime.now(),
+                                  photoUrl: ''),
+                              messages: {},
+                              createdAt: DateTime.now(),
+                            ));
+                if (existingConversation.conversationId == -1) {
+                  // Create new conversation
+                  chatBloc.add(
+                    NewConversationEvent(currentUser, user.id),
+                  );
+                } else {
+                  // Open existing conversation
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          ConversationPage(conversation: existingConversation),
+                    ),
+                  );
+                }
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }
